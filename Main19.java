aee77e36fda83b37316fd36813a4b906331b01ae
Aditta Das <adittadas214@gmail.com>
2025-08-08 22:25:05 +0600

    Add files via upload

---
+++ Main19.java
@@ -0,0 +1,7 @@
+public class Main19 {
+  public static void main(String[] args) {
+    /* The code below will print the words Hello World
+    to the screen, and it is amazing */
+    System.out.println("Hello World");
+  }
+}
